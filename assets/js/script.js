354158a7df1eb09d4e142c818cc0dd799a226c33
LWSE13 <leewheeler888@gmail.com>
2024-02-18 15:55:30 +0000

    added current time and day to my scheduler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+$(document).ready(function() {
+  function timeUpdate () {
+      var currentDate = dayjs().format('YYYY-MM-DD');
+      var currentTime = dayjs().format('h:mm:ss A');
+      $('#current-date-time').text('Current date: ' + currentDate + ', Current time: ' + currentTime);
+  }
+
+  timeUpdate();
+  setInterval(timeUpdate, 1000);
+});
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
