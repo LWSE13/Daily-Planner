250cb890a98fc58f8b35389d7b4c3e59676235b5
LWSE13 <leewheeler888@gmail.com>
2024-02-20 17:48:34 +0000

    adjusted updateClasses function. Begun work on saving and retrieving items from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
   function timeUpdate () {
-      var currentDate = dayjs().format('YYYY-MM-DD');
-      var currentTime = dayjs().format('h:mm:ss A');
-      $('#current-date-time').text('Current date: ' + currentDate + ', Current time: ' + currentTime);
+      var currentDate = dayjs().format("YYYY-MM-DD");
+      var currentTime = dayjs().format("h:mm:ss A");
+      $("#current-date-time").text("Current date: " + currentDate + ", Current time: " + currentTime);
   }
 
   timeUpdate();
   setInterval(timeUpdate, 1000);
 });
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -32,32 +30,29 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
 });
 
-
-
-//when document is ready run the following function
 $(document).ready(function() {
   function updateClasses() {
-    //creates a variable that uses dayjs to get the current hour
     var currentHour = dayjs().hour();
-    //selects each time block class and runs the following function
-    $('.time-block').each(function() {
-      //creates a variable timeBlockHour that uses parse to convert the id from a string to an integer
-      var timeBlockHour = parseInt($(this).attr('id'));
-      //if timeBlockHour is less than the current hour
+    $(".time-block").each(function() {
+      var timeBlockHour = parseInt($(this).attr("id"));
       if (timeBlockHour < currentHour) {
-        //$ selects this (the current time block) and adds the past class and removes the other classes 
-        $(this).addClass('past').removeClass('present future');
-        //code repeated for equal using an else if statement 
+        $(this).addClass("past").removeClass("present future");
       } else if (timeBlockHour === currentHour) {
-        $(this).addClass('present').removeClass('past future');
+        $(this).addClass("present").removeClass("past future");
       } else {
-        $(this).addClass('future').removeClass('past present');
+        $(this).addClass("future").removeClass("past present");
       }
     });
   }
-
   updateClasses();
-  //updates every hour (1 second x 60 seconds x 60 minutes = 1 hour or 3600000 milliseconds)
   setInterval(updateClasses, 1000 * 60 * 60); 
 });
 
+//selects the class saveBtn and adds a click event listener to it
+$(".saveBtn").click(function() {
+  var timeBlockSaves = JSON.parse(localStorage.getItem("timeBlockSaves")) || {};
+  var timeBlockID = $(this).parent().attr("id");
+  var timeBlockInput = $(this).siblings("textarea").val();
+  
+
+})
