622bd8ad4be591e1def811158e517a1869b9e3e6
LWSE13 <leewheeler888@gmail.com>
2024-02-20 19:42:39 +0000

    finished functions that saves and retrieves objects that contain text input corresponding to the correct time block id

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,26 +9,7 @@ $(document).ready(function() {
   setInterval(timeUpdate, 1000);
 });
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+
 
 $(document).ready(function() {
   function updateClasses() {
@@ -46,13 +27,22 @@ $(document).ready(function() {
   }
   updateClasses();
   setInterval(updateClasses, 1000 * 60 * 60); 
+
+  var timeBlockSaves = JSON.parse(localStorage.getItem("timeBlockInput"))
+  for (var timeBlockID in timeBlockSaves) {
+  
+    $("#" + timeBlockID).find("textarea").val(timeBlockSaves[timeBlockID]);
+  }
 });
 
-//selects the class saveBtn and adds a click event listener to it
-$(".saveBtn").click(function() {
-  var timeBlockSaves = JSON.parse(localStorage.getItem("timeBlockSaves")) || {};
-  var timeBlockID = $(this).parent().attr("id");
-  var timeBlockInput = $(this).siblings("textarea").val();
-  
 
+$(".saveBtn").click(function() {
+  var timeBlockSaves = JSON.parse(localStorage.getItem("timeBlockInput")) || {};
+  var timeBlockID = $(this).parent().attr("id");
+  var timeBlockText = $(this).siblings("textarea").val();
+  timeBlockSaves[timeBlockID] = timeBlockText;
+  localStorage.setItem("timeBlockInput", JSON.stringify(timeBlockSaves));
+  
 })
+
+
